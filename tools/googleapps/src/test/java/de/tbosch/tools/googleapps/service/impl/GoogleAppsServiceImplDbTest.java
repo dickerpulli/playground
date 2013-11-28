16f17c08a9435eacb63ebffa45de23acf48dde8e
Thomas Bosch <tbosch@gmx.de>
2013-11-28 12:58:41 +0100

    delete testcase

--- tools/googleapps/src/test/java/de/tbosch/tools/googleapps/service/impl/GoogleAppsServiceImplDbTest.java
+++ tools/googleapps/src/test/java/de/tbosch/tools/googleapps/service/impl/GoogleAppsServiceImplDbTest.java
@@ -1,11 +1,6 @@
 package de.tbosch.tools.googleapps.service.impl;
 
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.expectLastCall;
-import static org.junit.Assert.assertEquals;
-
 import java.io.IOException;
-import java.util.List;
 
 import org.easymock.classextension.EasyMock;
 import org.junit.Before;
@@ -13,10 +8,8 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import de.tbosch.tools.googleapps.AbstractSpringDbTest;
-import de.tbosch.tools.googleapps.model.GReminder;
 import de.tbosch.tools.googleapps.service.GoogleAppsService;
 import de.tbosch.tools.googleapps.service.PreferencesService;
-import de.tbosch.tools.googleapps.service.PreferencesService.PrefKey;
 
 public class GoogleAppsServiceImplDbTest extends AbstractSpringDbTest {
 
@@ -36,15 +29,7 @@ public class GoogleAppsServiceImplDbTest extends AbstractSpringDbTest {
 
 	@Test
 	public void testUpdateCalendar() throws Exception {
-		expect(preferencesServiceMock.readPref(PrefKey.USERNAME)).andReturn("usr").times(2);
-		expect(preferencesServiceMock.readPref(PrefKey.PASSWORD)).andReturn("pwd");
-		expectLastCall();
 
-		googleAppsService.connect();
-		googleAppsService.updateCalendar();
-		List<GReminder> reminders = googleAppsService.getAllReminders();
-
-		assertEquals(2, reminders.size());
 	}
 
 }
