d0df28256c23debecebd08bd669f21b0d73d539c
Thomas Bosch <tbosch@gmx.de>
2014-03-03 13:51:37 +0100

    status history

--- batch/src/main/java/de/tbosch/batch/controller/BatchController.java
+++ batch/src/main/java/de/tbosch/batch/controller/BatchController.java
@@ -2,6 +2,7 @@ package de.tbosch.batch.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.collections.KeyValue;
 import org.apache.commons.collections.keyvalue.DefaultKeyValue;
@@ -11,13 +12,14 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobException;
 import org.springframework.batch.core.launch.NoSuchJobExecutionException;
+import org.springframework.batch.core.launch.NoSuchJobInstanceException;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -51,17 +53,53 @@ public class BatchController {
 		}
 	}
 
-	@RequestMapping(value = "/job/{id}/status", method = RequestMethod.GET)
+	@RequestMapping(value = "/job/status", method = RequestMethod.GET)
 	public @ResponseBody
-	String status(@PathVariable String id) {
+	String status() {
 		try {
-			String text = jobOperator.getSummary(Long.valueOf(id));
-			Summary summary = Summary.parse(text);
-			return summary.getAttribute("status").toString();
-		} catch (NumberFormatException e) {
-			return "Job ID '" + id + "'must be a number";
-		} catch (NoSuchJobExecutionException e) {
-			return "Job with ID '" + id + "' was not found";
+			Set<Long> ids = jobOperator.getRunningExecutions(job.getName());
+			String status = "";
+			for (Long id : ids) {
+				try {
+					String text = jobOperator.getSummary(id);
+					Summary summary = Summary.parse(text);
+					status += summary.getAttribute("status").toString() + "<br/>";
+				} catch (NoSuchJobExecutionException e) {
+					status += "Job with ID '" + id + "' is not running any more<br/>";
+				}
+			}
+			return status;
+		} catch (NoSuchJobException e) {
+			return "Job with name '" + job.getName() + "' is not running";
+		}
+	}
+
+	@RequestMapping(value = "/job/history", method = RequestMethod.GET)
+	public @ResponseBody
+	String history() {
+		try {
+			List<Long> instanceIds = jobOperator.getJobInstances(job.getName(), 0, Integer.MAX_VALUE);
+			String history = "";
+			for (Long instanceId : instanceIds) {
+				try {
+					List<Long> ids = jobOperator.getExecutions(instanceId);
+					for (Long id : ids) {
+						try {
+							String text = jobOperator.getSummary(id);
+							Summary summary = Summary.parse(text);
+							history += "Execution ID '" + id + "': " + summary.getAttribute("status").toString()
+									+ "<br/>";
+						} catch (NoSuchJobExecutionException e) {
+							history += "Job with ID '" + id + "' is not running any more<br/>";
+						}
+					}
+				} catch (NoSuchJobInstanceException e1) {
+					return history += "Job instance with ID '" + instanceId + "' does not exist<br/>";
+				}
+			}
+			return history;
+		} catch (NoSuchJobException e) {
+			return "Job with name '" + job.getName() + "' has not been started yet";
 		}
 	}
 
