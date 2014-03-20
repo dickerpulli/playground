e3237a5b2ddaa6cf343cf4c7e3d20e755b88bd28
Thomas Bosch <tbosch@gmx.de>
2014-03-20 12:44:02 +0100

    JSP support

--- web/spring-boot/src/main/java/de/tbosch/web/springboot/EchoController.java
+++ web/spring-boot/src/main/java/de/tbosch/web/springboot/EchoController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EchoController {
@@ -13,6 +14,12 @@ public class EchoController {
 	@ResponseBody
 	public String echo(@PathVariable String value) {
 		return value;
+	}
+
+	@RequestMapping(value = "/echopage/{value}", method = RequestMethod.GET)
+	public String echopage(@PathVariable String value, ModelAndView modelAndView) {
+		modelAndView.addObject("value", value);
+		return "echo";
 	}
 
 }
