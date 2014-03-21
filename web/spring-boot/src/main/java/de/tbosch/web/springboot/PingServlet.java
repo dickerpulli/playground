38c692477522b87583e5a684145b0145b9dd262c
Thomas Bosch <tbosch@gmx.de>
2014-03-21 10:31:00 +0100

    uuid

--- web/spring-boot/src/main/java/de/tbosch/web/springboot/PingServlet.java
+++ web/spring-boot/src/main/java/de/tbosch/web/springboot/PingServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class PingServlet extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		ServletOutputStream os = resp.getOutputStream();
