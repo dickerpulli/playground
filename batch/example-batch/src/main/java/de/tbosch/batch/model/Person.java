9c16f9be7fae77953d2161076580c17fcb479380
Thomas Bosch <tbosch@gmx.de>
2014-04-11 18:55:36 +0200

    serializable

--- batch/example-batch/src/main/java/de/tbosch/batch/model/Person.java
+++ batch/example-batch/src/main/java/de/tbosch/batch/model/Person.java
@@ -1,9 +1,13 @@
 package de.tbosch.batch.model;
 
+import java.io.Serializable;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-public class Person {
+public class Person implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private String firstname;
 	private String lastname;
