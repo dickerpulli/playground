02d3754d42dfb0a4b9d722fc2cf4c4cf68541ac4
Thomas Bosch <tbosch@gmx.de>
2014-12-04 10:32:20 +0100

    implemented simple matrix

--- games/ocean/src/java/ocean/core/FishMatrixSimple.java
+++ games/ocean/src/java/ocean/core/FishMatrixSimple.java
@@ -1,41 +1,79 @@
 package ocean.core;
 
-import ocean.fish.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import ocean.fish.Fish;
+import ocean.fish.SimpleHerring;
+import ocean.fish.SimpleShark;
 
 public class FishMatrixSimple extends FishMatrix {
-	
-	public FishMatrixSimple( int height, int width )
-	{
-		super (height, width); 
+
+	public FishMatrixSimple(int height, int width) {
+		super(height, width);
 	}
-	
+
 	/**
 	 * Moves a single fish one step, possibly overwriting any fish the fish moves into.
-	 * @param fish The fish to set at the new location.
-	 * @param from The coordinate where the fish should be removed.
-	 * @param to The coordinate where the fish should be moved to.
+	 * 
+	 * @param fish
+	 *            The fish to set at the new location.
+	 * @param from
+	 *            The coordinate where the fish should be removed.
+	 * @param to
+	 *            The coordinate where the fish should be moved to.
 	 * 
 	 * @return The new coordinate of the fish.
 	 */
+	@Override
 	protected Coordinate moveFish(Fish fish, Coordinate from, Coordinate to) {
-		// TODO
-		return null;
+		setFish(from, null);
+		setFish(to, fish);
+		return to;
 	}
-	
-	
+
 	/**
-	 * Moves all fish one iteration, the most important method of this applet.
-	 * We iterate all rows from up to down and all columns from left to
-	 * right.<p>
+	 * Moves all fish one iteration, the most important method of this applet. We iterate all rows from up to down and
+	 * all columns from left to right.
+	 * <p>
 	 *
-	 * We maintain an array <code>moved</code> in order to remember if a fish
-	 * has already been moved. Otherwise some fish could move more than one
-	 * step in each phase.<p>
+	 * We maintain an array <code>moved</code> in order to remember if a fish has already been moved. Otherwise some
+	 * fish could move more than one step in each phase.
+	 * <p>
 	 */
-	public void moveAllFish ( ) {
-		// TODO
-	}   
-	
+	@Override
+	public void moveAllFish() {
+		Fish[] moved = new Fish[0];
+		for (int y = 0; y < getHeight(); y++) {
+			for (int x = 0; x < getWidth(); x++) {
+				Coordinate from = new Coordinate(x, y);
+				Fish fish = getFish(from);
+				if (fish != null && !fishAlreadyMoved(fish, moved)) {
+					// calculate next coordinate
+					Direction direction = fish.getDirection(from, this);
+					Coordinate offset = Coordinate.fromOffset(direction);
+					Coordinate to = new Coordinate((from.x + offset.x) % getWidth(), from.y + offset.y);
+
+					// move fish to next coordinate
+					moveFish(fish, from, to);
+
+					// save fish as already moved
+					moved = Arrays.copyOf(moved, moved.length + 1);
+					moved[moved.length - 1] = fish;
+				}
+			}
+		}
+	}
+
+	private boolean fishAlreadyMoved(Fish fish, Fish[] allMovedFish) {
+		for (Fish movedFish : allMovedFish) {
+			if (movedFish.equals(fish)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	/**
 	 * Fills the matrix with the number of SimpleHerings and SimpleSharks.
 	 * 
@@ -43,6 +81,13 @@ public class FishMatrixSimple extends FishMatrix {
 	 * @param numberSharks
 	 */
 	public void fillMatrix(int numberHerrings, int numberSharks) {
-		// TODO
+		LinkedList<Fish> manyFish = new LinkedList<Fish>();
+		for (int h = 0; h < numberHerrings; h++) {
+			manyFish.add(new SimpleHerring());
+		}
+		for (int s = 0; s < numberSharks; s++) {
+			manyFish.add(new SimpleShark());
+		}
+		fillMatrix(manyFish);
 	}
 }
---
+++ games/ocean/src/java/ocean/fish/SimpleShark.java
@@ -0,0 +1,96 @@
+package ocean.fish;
+
+import ocean.core.Coordinate;
+import ocean.core.Direction;
+import ocean.core.FishMatrix;
+import ocean.gui.ImageData;
+
+public class SimpleShark extends Fish {
+
+	private Direction lastDirection;
+	private Direction nextToLastDirection;
+
+	public SimpleShark() {
+		super(ImageData.SHARK);
+	}
+
+	public SimpleShark(ImageData imageData) {
+		super(imageData);
+	}
+
+	@Override
+	public Direction getDirection(Coordinate from, FishMatrix matrix) {
+		Direction move = Direction.STAY;
+
+		// initial direction is UP
+		if (lastDirection == null) {
+			lastDirection = Direction.UP;
+		}
+
+		// if last direction was stay, then move in oposite direction, if possible
+		if (lastDirection == Direction.STAY) {
+			if (nextToLastDirection == Direction.UP) {
+				Fish fishBelow = matrix.getFish(from, Direction.DOWN);
+				boolean anotherSharkIsBelow = fishBelow != null && fishBelow instanceof SimpleShark;
+				if (!anotherSharkIsBelow) {
+					move = Direction.DOWN;
+				} else {
+					// we have to simulate the last direction to make sure that nextToLastDirection isn't set to STAY
+					lastDirection = Direction.UP;
+					move = Direction.STAY;
+				}
+			} else if (nextToLastDirection == Direction.DOWN) {
+				Fish fishAbove = matrix.getFish(from, Direction.UP);
+				boolean anotherSharkIsAbove = fishAbove != null && fishAbove instanceof SimpleShark;
+				if (!anotherSharkIsAbove) {
+					move = Direction.UP;
+				} else {
+					// we have to simulate the last direction to make sure that nextToLastDirection isn't set to STAY
+					lastDirection = Direction.DOWN;
+					move = Direction.STAY;
+				}
+			}
+		}
+
+		// last UP: then move up once more or STAY if another shark is there or the surface has been reached
+		else if (lastDirection == Direction.UP) {
+			if (matrix.canMove(from, Direction.UP)) {
+				Fish fishAbove = matrix.getFish(from, Direction.UP);
+				boolean anotherSharkIsAbove = fishAbove != null && fishAbove instanceof SimpleShark;
+				if (anotherSharkIsAbove) {
+					move = Direction.STAY;
+				} else {
+					move = Direction.UP;
+				}
+			} else {
+				move = Direction.STAY;
+			}
+		}
+
+		// last DOWN: then move up once more or STAY if another shark is there or the surface has been reached
+		else if (lastDirection == Direction.DOWN) {
+			if (matrix.canMove(from, Direction.DOWN)) {
+				Fish fishAbove = matrix.getFish(from, Direction.DOWN);
+				boolean anotherSharkIsAbove = fishAbove != null && fishAbove instanceof SimpleShark;
+				if (anotherSharkIsAbove) {
+					move = Direction.STAY;
+				} else {
+					move = Direction.DOWN;
+				}
+			} else {
+				move = Direction.STAY;
+			}
+		}
+
+		// save the last move
+		nextToLastDirection = lastDirection;
+		lastDirection = move;
+		return move;
+	}
+
+	@Override
+	public String toString() {
+		return "This is a SimpleShark";
+	}
+
+}
