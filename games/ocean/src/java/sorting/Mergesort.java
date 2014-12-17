cea37e9fedae58b77e36f067443c14c8b1523e0f
Thomas Bosch <tbosch@gmx.de>
2014-12-17 08:37:07 +0100

    compare

--- games/ocean/src/java/sorting/Mergesort.java
+++ games/ocean/src/java/sorting/Mergesort.java
@@ -1,33 +1,63 @@
 package sorting;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class Mergesort<T> {
 
 	protected Comparator<T> comp;
-	private final T[] data;
 
-	public Mergesort(T[] newdata, Comparator<T> newcomp) {
+	public Mergesort(Comparator<T> newcomp) {
 		comp = newcomp;
-		data = newdata;
-		sort(0, data.length - 1);
-		newdata = data;
 	}
 
-	private T[] sort(int l, int r) {
+	public T[] sort(T[] data) {
+		return sort(0, data.length - 1, data);
+	}
+
+	private T[] sort(int l, int r, T[] data) {
 		if (l < r) {
 			int q = (l + r) / 2;
 
-			sort(l, q);
-			sort(q + 1, r);
-			merge(l, q, r);
+			sort(l, q, data);
+			sort(q + 1, r, data);
+			merge(l, q, r, data);
 		}
 		return data;
 	}
 
-	private void merge(int l, int q, int r) {
-		Comparable<T>[] leftdata = new Comparable[data.length];
-
+	private void merge(int l, int q, int r, T[] data) {
+		T[] arr = (T[]) new Object[data.length];
+		int i, j;
+		// copy left part to new array
+		for (i = l; i <= q; i++) {
+			arr[i] = data[i];
+		}
+		// copy right part to new array
+		for (j = q + 1; j <= r; j++) {
+			arr[r + q + 1 - j] = data[j];
+		}
+		i = l;
+		j = r;
+		// run compare method
+		for (int k = l; k <= r; k++) {
+			if (comp.compare(arr[i], arr[j]) <= 0) {
+				data[k] = arr[i];
+				i++;
+			} else {
+				data[k] = arr[j];
+				j--;
+			}
+		}
 	}
 
+	public static void main(String[] args) {
+		Mergesort<Integer> mergesort = new Mergesort<Integer>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1.compareTo(o2);
+			}
+		});
+		System.out.println(Arrays.toString(mergesort.sort(new Integer[] { 2, 3, 4, 1, 6, 2, 5, 0 })));
+	}
 }
