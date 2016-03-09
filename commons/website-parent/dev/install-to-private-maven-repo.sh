96f0c086df4336b0bc975c8ebbc4acb62b741698
Thomas Bosch <dickerpulli@gmail.com>
2016-03-09 07:35:29 +0100

    parent

---
+++ commons/website-parent/dev/install-to-private-maven-repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Muss im root-Verzeichnis des Projekts ausgeführt werden
+
+LIB=website-parent-1.0.0
+REPO=/home/thomas/Entwicklung/git/dickerpulli/maven-repo/
+
+# Main Install
+mvn clean install
+
+# Main Sources
+mvn install:install-file -DlocalRepositoryPath=$REPO -DcreateChecksum=true -Dfile=pom.xml -DpomFile=pom.xml
+
+# git
+cd $REPO
+git add .
+git commit -m "new release $LIB"
+git push
