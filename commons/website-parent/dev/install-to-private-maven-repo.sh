2f40431ccf189b6170d5b6fa552ef4e8bd2c7c2d
Thomas <thomas.bosch@codecentric.de>
2017-11-23 08:48:51 +0100

    update newrelic agent

--- commons/website-parent/dev/install-to-private-maven-repo.sh
+++ commons/website-parent/dev/install-to-private-maven-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Muss im root-Verzeichnis des Projekts ausgeführt werden
 
-LIB=website-parent-1.4.0
+LIB=website-parent-1.4.1
 REPO=/Users/thomas/Entwicklung/git/dickerpulli/maven-repo/
 
 # Main Install
