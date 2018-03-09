bb92ada7c97502e08d665fda1652f3899c649a3e
Thomas Bosch <tbosch@gmx.de>
2018-03-09 11:00:23 +0100

    Java9

--- commons/website-parent/dev/install-to-private-maven-repo.sh
+++ commons/website-parent/dev/install-to-private-maven-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Muss im root-Verzeichnis des Projekts ausgeführt werden
 
-LIB=website-parent-2.0.0
+LIB=website-parent-2.0.1
 REPO=/Users/thomas/Entwicklung/git/dickerpulli/maven-repo/
 
 # Main Install
